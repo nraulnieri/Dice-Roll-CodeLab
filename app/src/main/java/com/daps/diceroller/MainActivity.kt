7ccb011ee80bf2e4667f5aa269503ae18efa954b
nraulnieri <nraulnieri@gmail.com>
2022-02-09 12:33:46 -0500

    añadiendo imagenes para mostrar el numero random del 1 al 6

--- app/src/main/java/com/daps/diceroller/MainActivity.kt
+++ app/src/main/java/com/daps/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.daps.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -12,14 +13,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton:Button = findViewById(R.id.roll_button)
-        val bt_contar:Button = findViewById(R.id.bt_contar)
+        //val bt_contar:Button = findViewById(R.id.bt_contar)
 
         rollButton.setOnClickListener { rollDice() }
-        bt_contar.setOnClickListener { sumarUno() }
+        //bt_contar.setOnClickListener { sumarUno() }
     }
 
     //Sumar Uno incrementa el numero en 1, si es texto es igual 1, si es 6 se mantiene igual.
-    private fun sumarUno() {
+    /*private fun sumarUno() {
         val resultText: TextView = findViewById(R.id.result_text)
         var resultTextValue = resultText.text.toString()
 
@@ -31,13 +32,25 @@ class MainActivity : AppCompatActivity() {
             }
         }
         resultText.text = resultTextValue
-    }
+    }*/
 
     //Rodar un dado, genera valores aleatorios entre 1 y 6
     private fun rollDice(){
         val randomInt = (1..6).random()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        val diceImage:ImageView = findViewById(R.id.dice_image)
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+
+        /*val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()*/
         /*Toast.makeText(this, "button clicked",
             Toast.LENGTH_SHORT).show()*/
     }
